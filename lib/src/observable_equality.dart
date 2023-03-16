db2f9608d4ee681516ef1458977f681e1016fe03
beeth0ven <beeth0vendev@gmail.com>
2023-03-16 15:17:25 +0800

    refactor - add equality `PipeObservableEquality`

--- lib/src/observable_equality.dart
+++ lib/src/observable_equality.dart
@@ -25,3 +25,32 @@ class FallbackObservableEquality<T> implements Equality<Observable<T>> {
     return o is Observable<T>;
   } 
 }
+
+@internal
+@visibleForTesting
+class PipeObservableEquality<T, R> implements Equality<PipeObservable<T, R>> {
+  const PipeObservableEquality(this.getObservableEquality);
+
+  final Getter<Equality<Observable<T>>> getObservableEquality;
+
+  @override
+  bool equals(PipeObservable<T, R> e1, PipeObservable<T, R> e2) {
+    return identical(e1, e2)
+      || e1.runtimeType == e2.runtimeType
+        && getObservableEquality().equals(e1.source, e2.source);
+  }
+
+  @override
+  int hash(PipeObservable<T, R> e) {
+    return Object.hash(
+      e.runtimeType,
+      getObservableEquality().hash(e.source),
+    );
+  }
+
+  @override
+  bool isValidKey(Object? o) {
+    return o is PipeObservable<T, R>;
+  }
+  
+}
--- test/flutter_scope/observable_equality_test.dart
+++ test/flutter_scope/observable_equality_test.dart
@@ -7,6 +7,10 @@ final _observableCreate1 = Observable<int>((_) => Disposable.empty);
 final _observableCreate2 = Observable<int>((_) => Disposable.empty);
 final _observableMap1 = _observableCreate1.map((it) => it * 2);
 final _observableMap2 = _observableCreate2.map((it) => it * 2);
+final _observableDistinct1 = _observableCreate1.distinct();
+final _observableDistinct2 = _observableCreate2.distinct();
+final _observableMapDistinct1 = _observableMap1.distinct();
+final _observableMapDistinct2 = _observableMap2.distinct();
 
 void main() {
 
@@ -91,5 +95,113 @@ void main() {
       expected,
     );
 
+  });
+
+  test('`pipeObservableEquality.isValidKey` verify objects', () {
+
+    final equality = PipeObservableEquality<int, int>(
+      () => const FallbackObservableEquality<int>()
+    );
+
+    final objects = [
+      true,
+      0,
+      '',
+      Observable<bool>((_) => Disposable.empty),
+      Observable<int>((_) => Disposable.empty),
+      Observable<bool>((_) => Disposable.empty)
+        .map((it) => it ? 1 : 0),
+      Observable<int>((_) => Disposable.empty)
+        .map((it) => it * 2),
+      Observable<int>((_) => Disposable.empty)
+        .map((it) => it * 2)
+        .distinct(),
+    ];
+
+    final expected = [
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      true,
+      true,
+    ];
+
+    expect(
+      objects
+        .map(equality.isValidKey)
+        .toList(),
+      expected,
+    );
+  });
+
+  test('`pipeObservableEquality.equals` comparing observables for equality', () {
+
+    final equality = PipeObservableEquality<int, int>(
+      () => const FallbackObservableEquality<int>()
+    );
+
+    final observables = [
+      [_observableMap1, _observableMap2],
+      [_observableMap1, _observableDistinct2],
+      [_observableMap1, _observableMapDistinct2],
+      [_observableDistinct1, _observableDistinct2],
+      [_observableDistinct1, _observableMapDistinct2],
+      [_observableMapDistinct1, _observableMapDistinct2],
+    ];
+
+    final expected = [
+      true,
+      false,
+      false,
+      true,
+      false,
+      true,
+    ];
+    
+    expect(
+      observables
+        .map((list) {
+          final observable1 = list[0] as PipeObservable<int, int>;
+          final observable2 = list[1] as PipeObservable<int, int>;
+          return equality.equals(observable1, observable2);
+        })
+        .toList(),
+      expected,
+    );
+
+  });
+
+  test('`pipeObservableEquality.hash` return same value when observables are equal', () {
+
+    final equality = PipeObservableEquality<int, int>(
+      () => const FallbackObservableEquality<int>()
+    );
+
+    final observables = [
+      [_observableMap1, _observableMap2],
+      [_observableDistinct1, _observableDistinct2],
+      [_observableMapDistinct1, _observableMapDistinct2],
+    ];
+
+    final expected = [
+      true,
+      true,
+      true,
+    ];
+
+    expect(
+      observables
+        .map((list) {
+          final observable1 = list[0] as PipeObservable<int, int>;
+          final observable2 = list[1] as PipeObservable<int, int>;
+          return equality.hash(observable1) == equality.hash(observable2);
+        })
+        .toList(),
+      expected,
+    );
+    
   });
 }
