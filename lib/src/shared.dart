c70f675139f5b6bb833d77e4aa5152645de48ee2
beeth0ven <beeth0vendev@gmail.com>
2023-03-23 10:46:46 +0800

    remove unused helper methods

--- lib/src/shared.dart
+++ lib/src/shared.dart
@@ -1,30 +1,4 @@
 
-import 'package:meta/meta.dart';
 import 'package:flutter/widgets.dart';
-import 'package:dart_scope/dart_scope.dart';
-
-import 'flutter_scope.dart';
 
 typedef FlutterEqual<T> = T Function(BuildContext context);
-
-@internal
-FlutterEqual<States<T>> contextGetStates<T>({ 
-  required Object? name,
-}) {
-  return (context) => context.scope
-    .get<States<T>>(name: name);
-}
-
-@internal
-FlutterEqual<States<R>> contextConvertStates<T, R>({ 
-  required Object? name,
-  required R Function(T state) convert,
-  required Equals<R>? equals,
-}) {
-  return (context) => context.scope
-    .get<States<T>>(name: name)
-    .convert<R>(
-      convert,
-      equals: equals,
-    );
-}
