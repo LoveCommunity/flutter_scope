dfef2d2b449e80ee394dbd7374953f571a307a9a
beeth0ven <beeth0vendev@gmail.com>
2023-03-28 11:00:33 +0800

    add equality `configurableListEquality`

---
+++ lib/src/configurable_equality.dart
@@ -0,0 +1,28 @@
+
+
+import 'package:collection/collection.dart';
+import 'package:meta/meta.dart';
+
+@internal
+class RuntimeTypeEquality implements Equality<Object?> {
+  const RuntimeTypeEquality();
+  
+  @override
+  bool equals(Object? e1, Object? e2) {
+    return identical(e1, e2) || 
+      e1?.runtimeType == e2?.runtimeType;
+  }
+
+  @override
+  int hash(Object? e) {
+    return e?.runtimeType.hashCode ?? null.hashCode;
+  }
+
+  @override
+  bool isValidKey(Object? o) {
+    return true;
+  }
+}
+
+@internal
+const configurableListEquality = ListEquality(RuntimeTypeEquality());
